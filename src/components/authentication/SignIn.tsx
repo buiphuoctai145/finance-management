72b0faa9b7ca95d2435c5ca28c0f95294537a6e2
buiphuoctai145 <55799927+buiphuoctai145@users.noreply.github.com>
2023-08-02 19:35:04 +0700

    reverse sign in, sign up

--- src/components/authentication/SignIn.tsx
+++ src/components/authentication/SignIn.tsx
@@ -1,63 +1,10 @@
-// src/SignIn.tsx
 import React from 'react';
-import { useForm, Controller } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import { useAuth } from '../hooks/AuthContext';
 
-
-// Validation schema for sign-in form
-const validationSchema = yup.object().shape({
-    username: yup.string().required('Username is required'),
-    password: yup.string().required('Password is required'),
-  });
-  
-  // Define the type for the SignIn form data
-  interface SignInFormData {
-    username: string;
-    password: string;
-  }
-
-  
-const SignIn: React.FC = () => {
-  const { signIn } = useAuth();
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<SignInFormData>({
-    resolver: yupResolver(validationSchema),
-  });
-
-  const onSubmit = async (data: SignInFormData) => {
-    const { username, password } = data;
-    await signIn(username, password);
-  };
-
+const SignIn = () => {
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>Username</label>
-        <Controller
-          name="username"
-          control={control}
-          defaultValue=""
-          render={({ field }) => <input {...field} />}
-        />
-        {errors.username && <span>{errors.username.message}</span>}
-      </div>
-      <div>
-        <label>Password</label>
-        <Controller
-          name="password"
-          control={control}
-          defaultValue=""
-          render={({ field }) => <input type="password" {...field} />}
-        />
-        {errors.password && <span>{errors.password.message}</span>}
-      </div>
-      <button type="submit">Sign In</button>
-    </form>
+    <div>
+      
+    </div>
   );
 };
 
--- src/components/authentication/SignUp.tsx
+++ src/components/authentication/SignUp.tsx
@@ -1,61 +1,11 @@
-// src/SignUp.tsx
 import React from 'react';
-import { useForm, Controller } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import { useAuth } from '../hooks/AuthContext';
 
-// Validation schema for sign-up form
-const validationSchema = yup.object().shape({
-    username: yup.string().required('Username is required'),
-    password: yup.string().required('Password is required'),
-  });
-  
-  // Define the type for the SignUp form data
-  interface SignUpFormData {
-    username: string;
-    password: string;
-  }
-  
-
-export const SignUp = () => {
-  const { signUp } = useAuth();
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<SignUpFormData>({
-    resolver: yupResolver(validationSchema),
-  });
-
-  const onSubmit = async (data: SignUpFormData) => {
-    const { username, password } = data;
-    signUp(username, password, );
-  };
-
+const SignUp = () => {
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>Username</label>
-        <Controller
-          name="username"
-          control={control}
-          defaultValue=""
-          render={({ field }) => <input {...field} />}
-        />
-        {errors.username && <span>{errors.username.message}</span>}
-      </div>
-      <div>
-        <label>Password</label>
-        <Controller
-          name="password"
-          control={control}
-          defaultValue=""
-          render={({ field }) => <input type="password" {...field} />}
-        />
-        {errors.password && <span>{errors.password.message}</span>}
-      </div>
-      <button type="submit">Sign Up</button>
-    </form>
+    <div>
+      
+    </div>
   );
 };
+
+export default SignUp;
